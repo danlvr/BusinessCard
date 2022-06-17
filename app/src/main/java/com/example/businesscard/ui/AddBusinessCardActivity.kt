a957c9ba89a96522715df42cb4b6c3bf2fd196e5
Danilo <85370214+danlvr@users.noreply.github.com>
2022-06-17 14:46:06 -0300

    Created MainViewModel and added it to the MainActivity

--- app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import com.example.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
+
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -3,10 +3,18 @@ package com.example.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.businesscard.App
 import com.example.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
