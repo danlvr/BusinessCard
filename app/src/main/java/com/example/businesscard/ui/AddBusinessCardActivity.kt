386f60055e0fdaeba466257b03da90a29a9ff72c
Danilo <85370214+danlvr@users.noreply.github.com>
2022-06-17 12:18:36 -0300

    Added new card screen layout

--- app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,10 +2,10 @@ package com.example.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.businesscard.databinding.ActivityMainBinding
+import com.example.businesscard.databinding.ActivityAddBusinessCardBinding
 
-class MainActivity : AppCompatActivity() {
-    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+class AddBusinessCardActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
